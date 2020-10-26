b11ea3477da85be6ce17a8dbd95259c6b9d7a142
Tung Tran <ttran1185@gmail.com>
2020-10-26 10:56:21 -0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Search from "./pages/search";
+import Nav from "./components/nav";
+import Footer from "./components/footer";
+import Wrapper from './components/wrapper';
+import "./App.css";
 
 function App() {
+  document.title = "Employee Directory";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Wrapper>
+        <Search />
+      </Wrapper>
+      <Footer />
     </div>
   );
 }
-
 export default App;
+
