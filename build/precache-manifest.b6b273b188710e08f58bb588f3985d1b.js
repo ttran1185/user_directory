7ab7fda89b21a7f285aab08e15c583f025efc81b
Tung Tran <ttran1185@gmail.com>
2020-10-26 11:30:27 -0700

    add

---
+++ build/precache-manifest.b6b273b188710e08f58bb588f3985d1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a81d08af5187e5e876fc87510b04cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e158211fbe8c633d52cb",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6e91c72da753fbb30e64",
+    "url": "/static/css/main.4038c085.chunk.css"
+  },
+  {
+    "revision": "e158211fbe8c633d52cb",
+    "url": "/static/js/2.4fb86b39.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.4fb86b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e91c72da753fbb30e64",
+    "url": "/static/js/main.c97af2e2.chunk.js"
+  },
+  {
+    "revision": "55c35f69c557312cec76",
+    "url": "/static/js/runtime-main.03517605.js"
+  }
+]);
